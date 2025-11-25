96762dcfcc50b1289b8db294ac150a6f58081369
Husnain Ali <husnainalimughal588@gmail.com>
2025-11-25 23:42:30 +0500

    Random Commit

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req,res) => {
-    res.send("hey from product manager")
+    res.send("Hello from Products Manager")
 })
 
 module.exports = router
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req,res) => {
-    res.send("hey from user")
+    res.send("Hello from Users")
 })
 
 module.exports = router
