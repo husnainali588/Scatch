1a1fd87e02ec0ac5a3df01fab7a85d463185784e
Husnain Ali <husnainalimughal588@gmail.com>
2025-11-26 02:32:11 +0500

    SetUp MongoDb Connection thru config

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -4,7 +4,7 @@ const config = require("config")
 const dbgr = require("debug")("development:mongoose")
 
 mongoose
-.connect(`${config.get("MONGODB_URI")}/`)
+.connect(`${config.get("MONGODB_URI")}/scatch`)
 .then(() => {
     dbgr("connected");
 })
