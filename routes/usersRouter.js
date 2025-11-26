05bb5e512a09b5b0e1fc23f4d1043ac1b5429d2b
Husnain Ali <husnainalimughal588@gmail.com>
2025-11-26 15:36:23 +0500

    fix(models):user model overwrite error

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,27 @@
 const express = require("express")
 const router = express.Router()
+const userModel = require("../models/user-model")
 
-router.get("/", (req,res) => {
+router.get("/", (req, res) => {
     res.send("Hello from Users")
+})
+
+router.post("/register", async (req, res) => {
+    try {
+        let { email, password, fullname } = req.body
+
+        let user = await userModel.create({
+            email,
+            password,
+            fullname,
+        })
+
+        res.send(user);
+
+    } catch (err) {
+        console.log(err.message);
+    }
+
 })
 
 module.exports = router
