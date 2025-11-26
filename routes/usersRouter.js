bd160745d6c245df5d1e847e92c68308e46b5033
Husnain Ali <husnainalimughal588@gmail.com>
2025-11-26 18:34:44 +0500

    fix: bcrypt user password thru error handling

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,24 +2,33 @@ const express = require("express")
 const router = express.Router()
 const userModel = require("../models/user-model")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 router.get("/", (req, res) => {
     res.send("Hello from Users")
 })
 
 router.post("/register", async (req, res) => {
+
+    let { email, password, fullname } = req.body
+
     try {
-        let { email, password, fullname } = req.body
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password,salt)
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, async (err, hash) => {
+                if (err) return res.send(err.message)
+                else {
+                    let user = await userModel.create({
+                        email,
+                        password: hashedPassword,
+                        fullname,
+                    })
+                    res.status(201);
 
-        let user = await userModel.create({
-            email,
-            password: hashedPassword,
-            fullname,
+                    let token = jwt.sign({ email, id: user_id }, "secret")
+                    res.send(token)
+                }
+            })
         })
-
-        res.status(201).send(user);
 
     } catch (err) {
         console.log(err.message);
