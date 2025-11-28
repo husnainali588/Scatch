1f8a480e84d837a54875a6747ee716ec07ca580a
Husnain Ali <husnainalimughal588@gmail.com>
2025-11-28 23:41:06 +0500

    feat: add ownerlogin UI

--- app.js
+++ app.js
@@ -2,22 +2,33 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const indexRouter = require("./routes/indexRouter")
+const index = require("./routes/index")
 const ownersRouter = require("./routes/ownerRouter")
 const productsRouter = require("./routes/productsRouter")
 const usersRouter = require("./routes/usersRouter")
+const flash = require("connect-flash")
+const expressSession = require("express-session")
 
 require("dotenv").config()
 
 const db = require("./config/mongoose-connection")
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+    expressSession({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.EXPRESS_SESSION_SECRET
+    })
+)
+app.use(flash())
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 
-app.use("/",indexRouter)
+app.use("/",index)
 app.use("/owner", ownersRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
