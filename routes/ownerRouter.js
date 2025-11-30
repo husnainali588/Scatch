b33e64f84671924e83f3783db0a301e89386479e
Husnain Ali <husnainalimughal588@gmail.com>
2025-12-01 02:27:29 +0500

    feat: add create products functionality for owner

--- routes/ownerRouter.js
+++ routes/ownerRouter.js
@@ -2,16 +2,16 @@ const express = require("express")
 const router = express.Router()
 const ownerModel = require("../models/owner-model")
 
-if(process.env.NODE_ENV === "development"){
-    router.post("/create", async (req,res) => {
+if (process.env.NODE_ENV === "development") {
+    router.post("/create", async (req, res) => {
         let owners = await ownerModel.find()
-        if(owners.length > 0) {
+        if (owners.length > 0) {
             return res
-            .status(500)
-            .send("You don't have permission to create owner.")
+                .status(500)
+                .send("You don't have permission to create owner.")
         }
 
-        let {fullname, email, password} = req.body
+        let { fullname, email, password } = req.body
 
         let createdOwner = await ownerModel.create({
             fullname,
@@ -23,10 +23,9 @@ if(process.env.NODE_ENV === "development"){
     })
 }
 
-router.get("/admin", (req,res) => {
-    res.render("admin")
+router.get("/admin", (req, res) => {
+    let success = req.flash("success")
+    res.render("createproducts", { success })
 })
-
-
 
 module.exports = router 
