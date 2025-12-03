46fc5a34f05ea9a48324df4e9de97f44b6129314
Husnain Ali <husnainalimughal588@gmail.com>
2025-12-03 20:52:07 +0500

    feat: cart in progess

--- routes/index.js
+++ routes/index.js
@@ -19,15 +19,17 @@ router.get("/shop", isloggedin, async (req, res) => {
 
 router.get("/cart", isloggedin, async (req, res) => {
 
-    let products = await productModel.find()
-    res.render("cart", { products })
+    let user = await userModel
+        .findOne({ email: req.user.email })
+        .populate("cart");
+    res.render("cart", { user })
 })
 
 router.get("/addtocart/:id", isloggedin, async (req, res) => {
-    let user = await userModel.findOne({email: req.user.email})
+    let user = await userModel.findOne({ email: req.user.email })
     user.cart.push(req.params.id)
     await user.save()
-    req.flash("success","Product is added")
+    req.flash("success", "Product is added")
     res.redirect("/shop")
 })
 
